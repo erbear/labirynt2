5bb089c77372f85a57090adf79866b02e54b1a87
mikes01 <mikolaj0grygiel@gmail.com>
2013-05-24 12:10:21 +0200

    Poprawione rysowanie i dodany srand()

--- labirynt/LabiryntGen.cpp
+++ labirynt/LabiryntGen.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "StdAfx.h"
 #include "LabiryntGen.h"
 
@@ -95,4 +96,103 @@ void LabiryntGen::Generuj(Plansza *plansza)
 
 
 	}
+=======
+#include "StdAfx.h"
+#include "LabiryntGen.h"
+
+
+LabiryntGen::LabiryntGen(void)
+{
+}
+LabiryntGen::LabiryntGen(Plansza *plansza)
+{
+	v = plansza->GetPoziom();
+	n=v*v;
+	odwiedzony = new int[n+1];
+	for (int i = 0; i<n+1;i++)
+	odwiedzony[i] = 0;
+	graniczacy = new int[n+1];
+	for (int i = 0; i<n+1;i++)
+	graniczacy[i] = 0;
+}
+
+
+
+LabiryntGen::~LabiryntGen(void)
+{
+	delete[] odwiedzony;
+	odwiedzony = 0;
+	delete[] graniczacy;
+	graniczacy = 0;
+}
+
+void LabiryntGen::Generuj(Plansza *plansza)
+{
+	int z,x,y;
+	for (int i =0;i<n;i++)
+	{
+		if (i ==0)
+		{
+			z = rand() % n;
+			odwiedzony[z]=1;
+			graniczacy[z]=1;
+		}
+		else 
+		{
+			y = rand() % graniczace.size();
+			z=graniczace[y];
+			odwiedzony[graniczace[y]] = 1;
+			graniczace.erase(graniczace.begin() + (y));
+		}
+		vector <int> dopolaczenia;
+		if (z > 1 ){
+			if ((z%v)!=1)
+			{
+				if (graniczacy[z-1]==0)
+				{
+				
+					graniczace.push_back(z-1);
+					graniczacy[z-1] = 1;
+				}else if (odwiedzony[z-1]==1)
+						dopolaczenia.push_back(z-1);
+			}
+			if (z>v) {
+				if (graniczacy[z-v]==0)
+				{
+					graniczace.push_back(z-v);
+					graniczacy[z-v] = 1;
+				}
+				else if (odwiedzony[z-v]==1)
+					dopolaczenia.push_back(z-v);
+			}
+		}
+		if (z+1 <= n){
+			if (z%v!=0)
+			{
+				if (graniczacy[z+1]==0)
+				{
+					graniczace.push_back(z+1);
+					graniczacy[z+1] = 1;
+				} else if (odwiedzony[z+1]==1)
+						dopolaczenia.push_back(z+1);
+			}
+			if (z+v <= n){
+				if (graniczacy[z+v]==0)
+				{
+					graniczace.push_back(z+v);
+					graniczacy[z+v] = 1;
+				} else if (odwiedzony[z+v]==1)
+						dopolaczenia.push_back(z+v);
+			}
+		}
+		if (dopolaczenia.size()!=0)
+		{
+		x = rand() % dopolaczenia.size();
+		plansza->SetDroga(dopolaczenia[x], z);
+		}
+		
+
+
+	}
+>>>>>>> de391c487e26461975fa516e93e0e643d38c2c9e
 }
--- labirynt/obszar.cpp
+++ labirynt/obszar.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "StdAfx.h"
 #include "obszar.h"
 
@@ -100,4 +101,108 @@ void Obszar::Postaw(CDC *dc, Obiekt *obiekt)
 {
 	obiekt->setWierzcholki(wspolrzedna[0],wspolrzedna[1],wspolrzedna[4],wspolrzedna[5]);
 	obiekt->Rysuj(dc);
+=======
+#include "StdAfx.h"
+#include "obszar.h"
+
+
+Obszar::Obszar(void)
+{
+	sciana = new int[2];
+	wspolrzedna = new int [8];
+	kolor[0] = 255;
+	kolor[1] = 255;
+	kolor[2] = 153;
+}
+
+
+Obszar::~Obszar(void)
+{
+	delete []sciana;
+	delete []wspolrzedna;
+}
+void Obszar::SetSciana(int ktora)
+{
+	sciana[ktora]=1;
+}
+int Obszar::GetSciana(int ktora)
+{
+	return sciana[ktora];
+}
+void Obszar::DeleteSciana(int ktora)
+{
+	sciana[ktora]=0;
+}
+void Obszar::SetWspolrzedne(int x, int y, int bok)
+{
+	//lewy gorny wierzcholek
+	wspolrzedna[0] = x;
+	wspolrzedna[1] = y;
+
+	x+=bok;//przesuniecie x w prawo o bok kwadratu
+	
+	//prawy gorny wierzcholek
+	wspolrzedna[2] = x;
+	wspolrzedna[3] = y;
+	
+	y+=bok;//przesuniecie y w dol o bok kwadratu
+
+	//prawy dolny wierzcholek
+	wspolrzedna[4] = x;
+	wspolrzedna[5] = y;
+	
+	x-=bok;// przesuniecie x w lewo o bok kwadratu
+
+	//lewy dolny wierzcholek
+	wspolrzedna[6] = x;
+	wspolrzedna[7] = y;
+}
+
+int Obszar::GetWspolrzedna(int ktora)
+{
+	return wspolrzedna[ktora];
+}
+void Obszar::Maluj(CDC *dc)
+{
+	POINT p1,p2;
+	CRgn Rgn;
+	Rgn.CreateRectRgn(wspolrzedna[0],wspolrzedna[1],wspolrzedna[4],wspolrzedna[5]);
+	
+	CBrush brush(RGB(kolor[0], kolor[1], kolor[2]));
+
+	dc->FillRgn(&Rgn, &brush);
+	if (sciana[0] == 1)
+		{
+			p1.x=wspolrzedna[0];
+			p1.y=wspolrzedna[1];
+			p2.x=wspolrzedna[6];
+			p2.y=wspolrzedna[7];
+			POINT tab[2] = {p1,p2};
+			dc->Polyline(tab,2);
+		} 
+		if (sciana[1] == 1){
+			
+			p1.x=wspolrzedna[0];
+			p1.y=wspolrzedna[1];
+			p2.x=wspolrzedna[2];
+			p2.y=wspolrzedna[3];
+			POINT tab[2] = {p1,p2};
+			dc->Polyline(tab,2);
+		}
+}
+void Obszar::Wyczysc(CDC *dc)
+{
+	rgn = new CRgn;
+	rgn->CreateRectRgn(wspolrzedna[0]+1,wspolrzedna[1]+1,wspolrzedna[4]-1,wspolrzedna[5]-1);
+	
+	CBrush brush(RGB(kolor[0], kolor[1], kolor[2]));
+
+	dc->FillRgn(rgn, &brush);
+	delete rgn;
+}
+void Obszar::Postaw(CDC *dc, Obiekt *obiekt)
+{
+	obiekt->setWierzcholki(wspolrzedna[0],wspolrzedna[1],wspolrzedna[4],wspolrzedna[5]);
+	obiekt->Rysuj(dc);
+>>>>>>> de391c487e26461975fa516e93e0e643d38c2c9e
 }
